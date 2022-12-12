c77ce083d520009ddbe33ae20ef94a1f4ca2a4b0
Halwoll <97443146+Halwoll@users.noreply.github.com>
2022-12-12 17:35:30 +0000

    going through the Makers bites

---
+++ hello.js
@@ -0,0 +1,20 @@
+console.log('Hello - I am a file!');
+
+console.log(2 + 2);
+
+const a = 4;
+const b = 6;
+
+console.log(a + b);
+console.log(a * b);
+
+
+const greeting = 'Hello ';
+const name = 'Makers';
+console.log(greeting + name);
+
+const hello = () => {
+  console.log('Hello!');
+};
+
+module.exports = hello;
---
+++ sayHello.js
@@ -0,0 +1,22 @@
+// accepts no arguments
+// const printMessage = () => {
+//   console.log('Hi there!');
+// };
+// accepts an argument
+// const printMessage = (message) => {
+//   console.log(message);
+// };
+
+// printMessage('Hi ther!e');
+
+
+// const getFour = (getFour) => {
+//   return '4';
+// };
+
+// const something = getFour;
+// console.log(something)
+
+const sayHello = () => {
+  console.log('Hello');
+};
